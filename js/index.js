57aa14da71eb7ad2b6f47b71476c33be2c1bfdc2
Taran <thetaranneeld@gmail.com>
2019-08-05 16:44:14 -0600

    finished webpage layout

--- js/index.js
+++ js/index.js
@@ -40,3 +40,75 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//Grab 'a' elements
+let aTags = document.getElementsByTagName('a');
+
+//Grab siteContent.nav key/value pairs
+let navKeys = Object.keys(siteContent.nav);
+let navValues = Object.values(siteContent.nav);
+
+//Set each 'a' tag to its designated key/value pair
+for (let i = 0; i < aTags.length; i++) {
+  let a = aTags[i];
+  a.setAttribute('id', navKeys[i]);
+  a.textContent = navValues[i];
+}
+
+//Grab 'cta' class elements
+let ctaText = document.getElementsByClassName('cta-text')[0];
+let ctaImg = document.getElementById('cta-img');
+
+//Grab siteContent.cta key/value pairs
+let ctaKeys = Object.keys(siteContent.cta);
+let ctaValues = Object.values(siteContent.cta);
+
+//Set each 'cta' class element to its designated key/value pair
+ctaText.childNodes[1].textContent = ctaValues[0];
+ctaText.childNodes[3].textContent = ctaValues[1];
+ctaImg.setAttribute('src', ctaValues[2]);
+
+//Main Content
+//Top Content
+let topContent = document.getElementsByClassName('top-content')[0];
+let topText1 = topContent.getElementsByClassName('text-content')[0];
+let topText2 = topContent.getElementsByClassName('text-content')[1];
+
+let mainContentKeys = Object.keys(siteContent["main-content"]);
+let mainContentValues = Object.values(siteContent["main-content"]);
+
+topText1.childNodes[1].textContent = mainContentValues[0];
+topText1.childNodes[3].textContent = mainContentValues[1];
+topText2.childNodes[1].textContent = mainContentValues[2];
+topText2.childNodes[3].textContent = mainContentValues[3];
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', mainContentValues[4]);
+
+//Bottom Content
+let bottomContent = document.getElementsByClassName('bottom-content')[0];
+let bottomText1 = bottomContent.getElementsByClassName('text-content')[0];
+let bottomText2 = bottomContent.getElementsByClassName('text-content')[1];
+let bottomText3 = bottomContent.getElementsByClassName('text-content')[2];
+
+bottomText1.childNodes[1].textContent = mainContentValues[5];
+bottomText1.childNodes[3].textContent = mainContentValues[6];
+bottomText2.childNodes[1].textContent = mainContentValues[7];
+bottomText2.childNodes[3].textContent = mainContentValues[8];
+bottomText3.childNodes[1].textContent = mainContentValues[9];
+bottomText3.childNodes[3].textContent = mainContentValues[10];
+
+//Contact
+let contactValues = Object.values(siteContent.contact);
+
+let contacts = document.getElementsByClassName("contact")[0];
+contacts.childNodes[1].textContent = contactValues[0];
+contacts.childNodes[3].textContent = contactValues[1];
+contacts.childNodes[5].textContent = contactValues[2];
+contacts.childNodes[7].textContent = contactValues[3];
+
+//Footer
+let footerValues = Object.values(siteContent.footer);
+
+let copyright = document.getElementsByTagName('footer')[0];
+copyright.childNodes[0].textContent = footerValues[0];
